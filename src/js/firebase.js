e60a5eaa8778065259812f4581560b5eb222ff17
tungatarovM <tungatarov.96@mail.ru>
2018-07-10 00:06:24 +0500

    add firebase config file

---
+++ src/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDx8ol-_zgQoNlHMAIdUTLpQUG85TFw_9Q",
+    authDomain: "database-362cb.firebaseapp.com",
+    databaseURL: "https://database-362cb.firebaseio.com",
+    projectId: "database-362cb",
+    storageBucket: "database-362cb.appspot.com",
+    messagingSenderId: "759304096773"
+  };
+export default config;
